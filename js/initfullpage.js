934a55cc3b3e170a6eb3ba2b7bba79858d894a35
DIDIx13 <DIDI.ameli@gmail.com>
2017-11-18 15:45:18 +0100

    Add files via upload

---
+++ js/initfullpage.js
@@ -0,0 +1,9 @@
+
+$('#fullpage').fullpage({
+    navigationPosition: 'right',
+    navigationTooltips: ['Countdown', 'Story'],
+    menu: '#menu',
+    controlArrows: true,
+    css3:true,
+    navigation:true
+});
